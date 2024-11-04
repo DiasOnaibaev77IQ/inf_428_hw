cb58adc9ff13ddb1ca92ee2e199dd14b3fd8b9f6
Dias Onaibaev <120043273+DiasOnaibaev77IQ@users.noreply.github.com>
2024-11-04 14:16:13 +0600

    answer for accepted task 1

--- Homework1/Part1/intersection_of_two_arrays.py
+++ Homework1/Part1/intersection_of_two_arrays.py
@@ -1,1 +1,14 @@
-
+class Solution(object):
+    def findLengthOfLCIS(self, nums):
+        lengthOfArray = len(nums)
+        count = 1
+        maximum = count
+        for i in range(0, lengthOfArray - 1):
+            if nums[i] < nums[i+1]:
+                count += 1
+            else:
+                count = 1
+            
+            maximum = max(count, maximum)
+        
+        return maximum
