b52778fb2c7c9e82916729aacf3510e58aef916f
Dias Onaibaev <120043273+DiasOnaibaev77IQ@users.noreply.github.com>
2024-11-04 15:27:51 +0600

    answer for task 3 of part 1

--- Homework1/Part1/merge_sorted_array.py
+++ Homework1/Part1/merge_sorted_array.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        for i in range(n):
+            nums1.remove(0)
+        nums1.extend(nums2)
+        nums1.sort()
